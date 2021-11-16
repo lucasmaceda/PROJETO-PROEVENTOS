e3a1e09a619cb837148dd533c6311141aa64a36f
Lucas Maceda Miranda <lucasmacedamiranda@gmail.com>
2021-11-16 18:25:20 -0300

    seção 2 e 3: Introdução ao .NET 6 e EF Core 6

---
+++ Back/src/ProEventos.API/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(
+    context => context
+                .UseSqlite(builder
+                    .Configuration
+                    .GetConnectionString("Default")
+                )
+);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at 
+// https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    
+    IApplicationBuilder applicationBuilder = 
+        app.UseSwaggerUI(c => 
+            c.SwaggerEndpoint(
+                "/swagger/v1/swagger.json", 
+                "ProEventos.API v1"
+            ));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
