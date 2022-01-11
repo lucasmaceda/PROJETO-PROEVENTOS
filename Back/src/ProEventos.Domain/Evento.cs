29d41c744e5452c9eefc4a90ff888d0a966b7bc3
Lucas Maceda Miranda <lucasmacedamiranda@gmail.com>
2022-01-10 22:27:08 -0300

    criação de enum e identity

--- Back/src/ProEventos.Domain/Evento.cs
+++ Back/src/ProEventos.Domain/Evento.cs
@@ -1,4 +1,6 @@
-﻿namespace ProEventos.Domain {
+﻿using ProEventos.Domain.Identity;
+
+namespace ProEventos.Domain {
     public class Evento
     {
         public int Id { get; set; }
@@ -9,6 +11,8 @@
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
         public IEnumerable<Lote>? Lotes { get; set; }
         public IEnumerable<RedeSocial>? RedesSociais { get; set; }
         public IEnumerable<PalestranteEvento>? PalestrantesEventos { get; set; }
---
+++ Back/src/ProEventos.Domain/Identity/User.cs
@@ -0,0 +1,15 @@
+using ProEventos.Domain.Enum;
+
+namespace ProEventos.Domain.Identity
+{
+    public class User
+    {
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+        public Titulo Titulo { get; set; }
+        public string Descricao { get; set; }
+        public Funcao Funcao { get; set; }
+        public string ImagemURL { get; set; }
+        public IEnumerable<UserRole> UserRoles { get; set; }
+    }
+}
